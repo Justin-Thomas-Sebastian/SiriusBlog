0dd31cb8df424597283f9d985cf2d6b0beed4bdd
Justin Sebastian <justinthomassebastian@gmail.com>
2022-04-21 15:21:42 -0600

    finish views exercises

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -5,6 +5,7 @@ import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
@@ -17,19 +18,24 @@ public class PostController {
 
     @GetMapping("/posts")
     public String index(Model model){
-        model.addAttribute("posts", postDao.findAll());
-        return "index";
+//        model.addAttribute("posts", postDao.findAll());   * part of jpa exercise*
+        ArrayList<Post> indexPosts = new ArrayList<>();
+        indexPosts.add(new Post("index post title 1", "index post body 1"));
+        indexPosts.add(new Post("index post title 2", "index post body 2"));
+        model.addAttribute("indexPosts", indexPosts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String getPostById(@PathVariable String id, Model model){
-        model.addAttribute("postId", postDao.findById(Long.valueOf(id)));
-        return "index";
+    public String getPostById(@PathVariable long id, Model model){
+        Post post = new Post("test title", "test body");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
     public String getCreateForm(){
-        return "create";
+        return "posts/create";
     }
 
     @PostMapping("/posts/create")
--- src/main/java/com/codeup/springblog/repositories/Post.java
+++ src/main/java/com/codeup/springblog/repositories/Post.java
@@ -13,5 +13,44 @@ public class Post {
     private String title;
 
     @Column(nullable = false)
-    private String description;
+    private String body;
+
+    public Post(){
+
+    }
+
+    public Post(String title, String body){
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
